6db9255b9b3f4994429179ddf28091989c64dbbc
EcheCostabel <exequielcostabel@gmail.com>
2023-02-18 15:26:40 -0300

    change border radius

--- src/App.jsx
+++ src/App.jsx
@@ -132,7 +132,7 @@ function App() {
           <div className="bg-[#1B2831] py-1 px-4 rounded-tr-xl rounded-br-xl rounded-bl-xl float-left mb-2 flex w-1/2">
             <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aliquam soluta cumque, nesciunt error expedita sed fugit? Sequi at molestias et facere repellat nobis laboriosam iste deleniti aut explicabo cumque, qui doloribus aliquid vero repudiandae, dignissimos voluptas accusantium alias consequuntur libero pariatur. Vitae assumenda, a est sunt dolor exercitationem quas quidem!</p>
           </div>
-          <div className="bg-[#054640] py-1 px-4 rounded-tr-xl rounded-br-xl rounded-bl-xl float-right mb-2 flex w-1/2">
+          <div className="bg-[#054640] py-1 px-4 rounded-tl-xl rounded-bl-xl rounded-br-xl float-right mb-2 flex w-1/2">
             <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aliquam soluta cumque, nesciunt error expedita sed fugit? Sequi at molestias et facere repellat nobis laboriosam iste deleniti aut explicabo cumque, qui doloribus aliquid vero repudiandae, dignissimos voluptas accusantium alias consequuntur libero pariatur. Vitae assumenda, a est sunt dolor exercitationem quas quidem!</p>
           </div>
         </main>
